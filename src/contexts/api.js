416d76bafefa22373eb9cd5b79d5302e9145e5b0
Hemanth <viruhemanth@gmail.com>
2021-05-13 22:20:15 +0530

    corrections

--- src/contexts/api.js
+++ src/contexts/api.js
@@ -6,17 +6,10 @@ const ApiContext = createContext({
   currentData: [],
   rem: [],
   city: [],
-  currentPage: 1,
-  totalPages: [],
-  firstIndex: [],
-  lastIndex: [],
-  tableData: [],
-  turnPage: () => {},
 });
 
 export function ApiProvider(props) {
   const [currentData, setCurrentData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
 
   const [rem, setRem] = useState([]);
   const [city, setCity] = useState([]);
@@ -41,12 +34,6 @@ export function ApiProvider(props) {
         currentData: currentData,
         rem: remdesivirData,
         city: cityWiseData,
-        totalPages: totalPages,
-        firstIndex: firstIndex,
-        lastIndex: lastIndex,
-        // turnPage: turnPage,
-        currentPage: currentPage,
-        tableData: tableData,
       }}
     >
       {!cityWiseData && !remdesivirData ? (
